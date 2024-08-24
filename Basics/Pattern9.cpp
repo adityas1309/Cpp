3fef2a6b4df9dc8841a757e510f437f070aa3d90
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-24 14:23:23 +0530

    Create Pattern9.cpp

---
+++ Basics/Pattern9.cpp
@@ -0,0 +1,39 @@
+/* Print this pattern
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number:";
+    cin >> n;
+    for(int i=1;i<=n/2;i++) {
+        for(int j=1;j<=(n/2)-i;j++) {
+            cout << " ";
+        }
+        for(int k=1;k<=(2*i-1);k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=(n/2);i>0;i--) {
+        for(int j=1;j<=(n/2)-i;j++) {
+            cout << " ";
+        }
+        for(int k=(2*i-1);k>0;k--) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
