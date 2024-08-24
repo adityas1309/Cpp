28f298ac70b4e2ad18325082941691399761aea7
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-24 13:49:01 +0530

    Create Pattern5.cpp

---
+++ Basics/Pattern5.cpp
@@ -0,0 +1,19 @@
+/* Print this pattern
+*****
+****
+***
+**
+*
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++) {
+        for(int j=5;j>=i;j--) {
+            cout << "*";
+        }
+        cout<< "\n";
+    }
+    return 0;
+}
