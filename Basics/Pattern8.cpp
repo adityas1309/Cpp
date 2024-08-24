6999650de2e2031e0dc919bf1295d7afbc2870eb
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-24 14:13:17 +0530

    Create Pattern8.cpp

---
+++ Basics/Pattern8.cpp
@@ -0,0 +1,25 @@
+/* Print this pattern
+*********
+ *******
+  *****
+   ***
+    *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number:";
+    cin >> n;
+    for(int i=5;i>0;i--) {
+        for(int j=1;j<=n-i;j++) {
+            cout << " ";
+        }
+        for(int k=(2*i-1);k>0;k--) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
